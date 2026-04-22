c84c433bdadc113f2a60e4f0d46a6c4762c68019
Nandhana Sunil <133036831+NandhanaSunil@users.noreply.github.com>
2026-04-22 18:29:40 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> charmap;
+        if(s.size() != t.size()) return false;
+        for(char c: s) charmap[c]++;
+        for(char c: t) charmap[c]--;
+        for(char c: s) if(charmap[c]) return false;
+        return true;
+    }
+};
